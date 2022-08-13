da1e334b447029b710a1a19d04251c892d03861f
TheYoungManLi <cjl@kubesphere.io>
2022-08-14 00:03:54 +0800

    init project
    Signed-off-by: TheYoungManLi <cjl@kubesphere.io>

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { Loading } from '@kube-design/components'
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const Render = (component) => {
+  return (
+    <Suspense fallback={<Loading className="ks-page-loading" />}>
+      {component}
+    </Suspense>
+  );
+}
+
+export default App;
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  Render(<App />)
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/EIP/index.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class EIP extends Component {
+  render() {
+    return (
+      <div>EIP</div>
+    )
+  }
+}
